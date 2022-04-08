f8908e445bfe18f93ecdd153d6e7b92f78f6c48e
Jack Bartlett <90886034+jack-bartlett@users.noreply.github.com>
2022-04-08 16:23:36 -0400

    comment

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -31,7 +31,7 @@ export default function Project() {
 
   return (
     <div>
-      {/* what we are passing as props */}
+      {/* what we are passing as props! */}
       {/* We are passing the currentPage from state and the function to update it */}
       <Header currentPage={currentPage} handlePageChange={handlePageChange} />
       {/* Here we are calling the renderPage method which will return a component  */}
